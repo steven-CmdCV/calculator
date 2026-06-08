60fed57edccb425b608846b2d66b3befded4d34b
Steven <steven@example.com>
2026-06-07 23:18:25 -0400

    Create basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,33 @@
+// Operator functions
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return a + b;
+      break;
+    case "-":
+      return a - b;
+      break;
+    case "/":
+      return a / b;
+      break;
+    default:
+      return "Please choose a valid operator";
+  }
+}
