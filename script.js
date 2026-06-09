c8c46f08449099e13006f0f9ce97391e87ff42a9
Steven <steven@example.com>
2026-06-09 17:41:33 -0400

    Add number and operator storing
    FirstNumber and secondNumbers are properly stored
    from user input when clicking on digit buttons.
    Operator is also stored correctly.
    
    These are stored in order to be passed as
    arguments in the operate() function and returning
    and populating the display with the result.

--- script.js
+++ script.js
@@ -40,16 +40,42 @@ function operate(operator, a, b) {
 function generateCalculatorButtons(n) {
   for (let i = 0; i < n; i++) {
     const button = document.createElement("button");
-    if (i === 0) button.textContent = "AC";
-    if (i > 0 && i < 10) button.textContent = i;
-    if (i === 10) button.textContent = 0;
-    if (i > 10 || i === 0) button.style.backgroundColor = "#FF9500";
-    if (i === 11) button.textContent = "+";
-    if (i === 12) button.textContent = "-";
-    if (i === 13) button.textContent = "x";
-    if (i === 14) button.textContent = "÷";
-    if (i === 15) button.textContent = "=";
-    if (i === 15) button.style.width = `${buttonContainer.offsetWidth - 8}px `;
+    if (i === 0) {
+      button.textContent = "AC";
+      button.dataset.type = "clear";
+    }
+    if (i > 0 && i < 10) {
+      button.textContent = i;
+      button.dataset.type = "digit";
+    }
+    if (i === 10) {
+      button.textContent = "0";
+      button.dataset.type = "digit";
+    }
+    if (i > 10 || i === 0) {
+      button.style.backgroundColor = "#FF9500";
+    }
+    if (i === 11) {
+      button.textContent = "+";
+      button.dataset.type = "digit";
+    }
+    if (i === 12) {
+      button.textContent = "-";
+      button.dataset.type = "digit";
+    }
+    if (i === 13) {
+      button.textContent = "x";
+      button.dataset.type = "operator";
+    }
+    if (i === 14) {
+      button.textContent = "÷";
+      button.dataset.type = "operator";
+    }
+    if (i === 15) {
+      button.style.width = `${buttonContainer.offsetWidth - 8}px `;
+      button.textContent = "=";
+      button.dataset.type = "equals";
+    }
     buttonContainer.appendChild(button);
   }
 }
@@ -60,6 +86,41 @@ const buttonContainer = document.getElementById("buttons");
 
 generateCalculatorButtons(16);
 
-// Placeholder for the display
+// Button functionality
 
-displayContainer.textContent = "123456";
+const buttons = buttonContainer.querySelectorAll("button");
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "AC") {
+      displayContainer.textContent = "";
+      firstNumber = "";
+      operator = "";
+      secondNumber = "";
+      console.log(button.dataset.type);
+    } else if (operator.length === 0 && button.dataset.type === "digit") {
+      displayContainer.textContent += button.textContent;
+      firstNumber += button.textContent;
+      console.log(`First Number: ${firstNumber}`);
+    } else if (button.dataset.type === "operator" && operator.length === 0) {
+      displayContainer.textContent += button.textContent;
+      operator += button.textContent;
+      console.log(`Operator: ${operator}`);
+    } else if (operator.length > 0 && button.dataset.type === "digit") {
+      displayContainer.textContent += button.textContent;
+      secondNumber += button.textContent;
+      console.log(`Second Number: ${secondNumber}`);
+    } else if (button.dataset.type === "equals") {
+      let operationResult = operate(
+        operator,
+        Number(firstNumber),
+        Number(secondNumber),
+      );
+      console.log(`Operation result: ${operationResult}`);
+    }
+  });
+});
